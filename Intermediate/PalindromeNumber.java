7795938743c077adb7c6a59b302d010288f60c28
Poorna Shri <poornashri2703@gmail.com>
2024-03-25 11:17:09 +0530

    task

--- Intermediate/PalindromeNumber.java
+++ Intermediate/PalindromeNumber.java
@@ -1,25 +1,39 @@
 import java.util.Scanner;
 
-public class PalindromeNumber{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-        boolean isPalindrome = isPalindrome(number);
-        if(isPalindrome){
-            System.out.println(number +"is a palindrome number.");
-        }else{
-            System.out.println(number + "is a palindrome number.")
-        }
-    }
-}
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-public static boolean isPalindrome(int number){
-    int reversedNumber = 0;
-    int originalNamuber = number;
-    while(number != 0){
-        int digit = number % 10;
-        reversedNumber = reversedNumber * 10 + digit;
-        number /= 10;
+        // Input the number
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is palindrome
+        boolean isPalindrome = isPalindrome(number);
+
+        // Print the result
+        if (isPalindrome) {
+            System.out.println(number + " is a palindrome number.");
+        } else {
+            System.out.println(number + " is not a palindrome number.");
+        }
+
+        scanner.close();
     }
-    return originalNumber == reversedNumber;
+
+    // Method to check if a number is palindrome
+    public static boolean isPalindrome(int number) {
+        int reversedNumber = 0;
+        int originalNumber = number;
+
+        // Reverse the number
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+
+        // Check if the original number is equal to its reverse
+        return originalNumber == reversedNumber;
+    }
 }
