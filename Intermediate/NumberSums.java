647daafb908c3dfb084dfa6b231279a8dad65696
Shri2703 <poornashri2703@gamil.com>
2024-04-04 14:01:40 +0530

    task

--- Intermediate/NumberSums.java
+++ Intermediate/NumberSums.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class NumberSums{
     public static void main(String[],args){
-        
+        Scanner input = new Scanner(System.in);
+        int negativeSum = 0;
+        int positiveEvenSum =0;
+        int positiveOddSum = 0;
+        System.out.println("Enter number (enter 0 to terminate):");
+        while(true){
+            int num = input.nextInt();
+
+            if(num ==0){
+                break;
+            }
+        }
+
     }
 }
