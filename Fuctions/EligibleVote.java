9d0988f27e0ec3619fd70cbeb2c1eb140d106e39
Shri2703 <poornashri2703@gamil.com>
2024-04-08 12:25:05 +0530

    task

--- Fuctions/EligibleVote.java
+++ Fuctions/EligibleVote.java
@@ -1,2 +1,12 @@
 import java.util.Scanner;
-public class 
+public class EligibleVote{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        int age = input.nextInt();
+        if(age>= 18){
+            System.out.println("Eligible to vote");
+        }else{
+             System.out.println(" Not Eligible to vote");
+        }
+    }
+}
