6994741fbf7d71335f0d0cc1685707f914c8df2c
Shri2703 <poornashri2703@gamil.com>
2024-04-15 13:28:07 +0530

    task

--- Fuctions/Factorial.java
+++ Fuctions/Factorial.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Factorial{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number:");
         int num = input.nextInt();
         int i,fact =1;
         fact = FactorialNum(num);
-        System.out.println("Factorial of" + num +"is :" + fact);  
+        System.out.println("Factorial of " + num +" is:" + fact);  
     }
     public static int FactorialNum(int num){
         if(num ==0){
