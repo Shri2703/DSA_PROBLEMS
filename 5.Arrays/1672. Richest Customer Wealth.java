e9244d13ad643ade30221246a5c3422334ac2e6a
Shri2703 <poornashri2703@gamil.com>
2024-05-07 07:26:57 +0530

    task

---
+++ 5.Arrays/1672. Richest Customer Wealth.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int wealth=0;
+        int max=0;
+        for (int i = 0; i < accounts.length; i++) {
+            
+            for (int j = 0; j < accounts[i].length; j++) {
+                wealth = wealth+accounts[i][j];
+            }
+            if (max<=wealth) {
+                max=wealth;
+            }
+            wealth=0;
+        }
+
+        return max;
+    }
+}
