9e2ed47816eabb420bf65080b1e54bd998c112ca
Shri2703 <poornashri2703@gmail.com>
2024-12-07 14:24:31 +0530

    task

--- TakeUForword/Step1/BasicOfRecursion/Basic.java
+++ TakeUForword/Step1/BasicOfRecursion/Basic.java
@@ -95,19 +95,89 @@ import java.util.Scanner;
 //     }
 // }
 
+// public class Basic{
+//     public static int factorial(int number){
+//         int ans = 1;
+//         for (int i = 1;i <= number;i++){
+//             ans = ans * i;
+//         }
+//         return ans;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter your number:");
+//         int number = sc.nextInt();
+//         int fact = factorial(number);
+//         System.out.println("The factorial of " + number+ " is " + fact);
+//     }
+// }
+
+// Enter your number:
+// 5
+// The factorial of 5 is 120
+
+// public class Basic{
+//     static void printarray(int ans[],int num){
+//         System.out.print("Resver array is :");
+//         for(int i =0; i< num; i++){
+//             System.out.print(ans[i] + " ");
+//         }
+
+//     }
+//     static void reversearray(int arr[],int num){
+//         int ans[] = new int[num];
+//         for(int i  = num -1;i>=0;i--){
+//             ans[num-i-1] = arr[i];
+//         }
+//         printarray(ans,num);
+//     }
+//     public static void main(String [] args){
+//         int[] arr ={2,4,6,8,10};
+//         int num = 5;
+//         reversearray(arr,num);
+//     }
+// }
+
+//Resver array is :10 8 6 4 2
+
+// public class Basic {
+//     public static void main(String[] args) {
+//         String str = "ABCBA"; // Change this to test other strings
+//         String rev = new StringBuilder(str).reverse().toString();
+        
+//         System.out.println("Original String: " + str);
+//         System.out.println("Reversed String: " + rev);
+        
+//         // Check if the string is a palindrome
+//         if (str.equals(rev)) {
+//             System.out.println("The string is a palindrome.");
+//         } else {
+//             System.out.println("The string is not a palindrome.");
+//         }
+//     }
+// }
+// Original String: ABCBA
+// Reversed String: ABCBA
+// The string is a palindrome
+
+
 public class Basic{
-    public static int factorial(int number){
-        int ans = 1;
-        for (int i = 1;i <= number;i++){
-            ans = ans * i;
+    public static void main(String... args){
+        int num = 8;
+        if(num == 0) System.out.println(0);
+        else{
+            int fib[] = new int[num+1];
+            fib[0] = 0;
+            fib[1] = 1;
+            for(int i = 2;i<num;i++){
+                fib[i] = fib[i-1]+fib[i-2];
+            }
+            System.out.println("The Fibonacci Series up to "+num+"th term:");
+            for(int i =0;i<= num;i++){
+                System.out.print(fib[i] +" ");
+            }
         }
-        return ans;
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your number:");
-        int number = sc.nextInt();
-        int fact = factorial(number);
-        System.out.println("The factorial of " + number+ " is " + fact);
     }
 }
+The Fibonacci Series up to 8th term:
+0 1 1 2 3 5 8 13 0
